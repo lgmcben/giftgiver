fff80e7b0d7924dd8f1a59961aaf55e1bd5a75fa
BEER-DELL\lgmcb <droidstick@gmail.com>
2019-02-18 18:08:58 +0700

    feat: Add a unit test for adding gift

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,16 @@ class App extends Component {
         return(
             <div>
                 <h2>Gift Giver 2</h2>
+                <div className='gift-list'>
+                    {
+                        this.state.gifts.map(gift => {
+                            return(
+                                <div key={gift.id}></div>
+                            )
+                            
+                        })
+                    }
+                </div>
                 <Button className='btn-add' onClick={this.addGift}>Add Gift</Button>
             </div>
         )
